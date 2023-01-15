778a78b86a3b1e3ff5a8cbd30798fb8c513da930
Dima Chachin <chachin99@bk.ru>
2023-01-15 14:25:01 +0300

    First two method

--- test/Program.cs
+++ test/Program.cs
@@ -3,3 +3,42 @@
 длинна которых меньше либо равна 3 символа. Первоначальный массив можноввести
 с клавиатуры, либо задать на старте выполнения алгоритма.
 */
+
+//функция получения числа с консоли
+int GetNumber(string message)
+{
+    int result = 0;
+    bool isCorrect = false;
+
+
+    while (!isCorrect)
+    {
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            isCorrect = true;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число или 0. Введите корректное число");
+        }
+    }
+
+    return result;
+}
+
+//Создание массива
+string[] createArray(int x)
+{
+    string[] array = new string[x];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"Введите элемент №{i}");
+
+        array[i] = Console.ReadLine();
+    }
+
+    return array;
+}
